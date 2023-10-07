334960382ef0dfeeb73f371b31e55858ff902341
bobo chow <bobchow0607@gmail.com>
2023-10-07 20:49:55 +0800

    add train.sh

---
+++ train.sh
@@ -0,0 +1,22 @@
+export CUDA_VISIBLE_DEVICES=2
+
+# python tools/train.py configs/recognition/aim/vitclip_base_hmdb51.py
+
+# python tools/train.py configs/recognition/aim/vitclip_tps_base_hmdb51.py
+
+# bash tools/dist_train.sh configs/recognition/aim/vitclip_base_sthv2.py
+
+# python tools/train.py configs/recognition/aim/vitclip_utuner_base_sthv2.py
+
+# python tools/train.py configs/recognition/uniformerv2/uniformerv2-base-p16-res224_clip_8xb32-u8_hmdb51.py
+
+
+# python tools/train.py configs/recognition/aim/vitclip_utuner_base_k400.py
+
+# python tools/train.py configs/recognition/aim/vitclip_utuner_base_diving48.py
+
+python tools/train.py configs/recognition/aim/vitclip_utuner_base_hmdb51.py
+
+# python tools/train.py configs/recognition/aim/vitclip_tome_base_hmdb51.py
+
+# python tools/train.py configs/recognition/aim/vitclip_ats_base_hmdb51.py
